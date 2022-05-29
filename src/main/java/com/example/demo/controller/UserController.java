1e3c1e622e3e80de6dcb12810adeaa5195f8cf97
Juan <juuaann1026@gmail.com>
2022-05-29 12:57:32 +0200

    Documentacion

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.domain.User;
 import com.example.demo.domain.Vuelo;
 import com.example.demo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,12 +27,23 @@ public class UserController {
 
     private final UserService userService;
 
-
+    @Operation(summary = "Obtiene el listado de usuarios")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listado de usuarios",
+                    content = @Content(array = @ArraySchema(schema =
+                    @Schema(implementation = Vuelo.class)))),
+    })
     @GetMapping("/users")
     public ResponseEntity<List<User>>getUsers(){
         return ResponseEntity.ok().body(userService.getUsers());
     }
 
+    @Operation(summary = "Introduce un usuario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Intoduccion de usuarios",
+                    content = @Content(array = @ArraySchema(schema =
+                    @Schema(implementation = Vuelo.class)))),
+    })
     @PostMapping("/user/save")
     public ResponseEntity<User>saveUser(@RequestBody User user){
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/vuelos/user/save").toUriString());
